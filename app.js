3b25cd92b86e1cb52cfa9954a5b585326aeb40bb
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:22:19 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+<script>
+        let myName = "Taro"; 
+        let guess = prompt("เจ้าของชื่อ"); 
+
+        if (guess === myName) {
+        alert("ถูกต้อง!"); 
+        } else {
+        alert("ผิดจ้า!"); 
+        }
+
+</script>
